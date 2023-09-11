57ef7412e6e27188d8990310028c35fba99ddcc7
21stPhenom <phenom.h.acks21@gmail.com>
2023-09-11 13:03:32 +0100

    updated settings and build script

--- hngx_stage_2/settings.py
+++ hngx_stage_2/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hngx-stage-2.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hngx-task-2-ui07.onrender.com']
 
 
 # Application definition

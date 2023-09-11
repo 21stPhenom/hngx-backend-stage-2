9686c595563aeaac75402a978a22b406b0b6739a
21stPhenom <phenom.h.acks21@gmail.com>
2023-09-11 12:53:37 +0100

    created Person model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(verbose_name="person name", max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
